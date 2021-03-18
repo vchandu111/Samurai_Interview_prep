82b28c9c23dc98e7e1556de6a84d8deb19eb3869
jayesh <jayeshkale052@gmail.com>
2021-03-18 21:55:03 +0530

    redem day 2

---
+++ Jayesh/day2.js
@@ -0,0 +1,12 @@
+// What is JSON and its common operations
+// What is Hoisting , explaining about var hoisting with examples
+// What is the main difference between localStorage and sessionStorage
+// What is the difference between null and undefined
+// What is the difference between an attribute and a property ? give examples
+// What are events and its types
+// How do you combine two or more arrays
+// What happens if we add two arrays? show with examples
+// Difference between alert and prompt ?
+
+
+
