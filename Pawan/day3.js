f18f2d3dc13c80b1624fe1da9448690f75bd6cec
Pawan Patidar <pawanpatidar21@gmail.com>
2021-03-24 09:11:56 +0530

    day3

--- Pawan/day3.js
+++ Pawan/day3.js
@@ -129,34 +129,3 @@ console.log(member.getFullName());
 
 
 
-
-// function sayHi() {
-//     console.log(name);
-//     console.log(age);
-//     var name = 'Lydia';
-//     let age = 21;
-//   }
-  
-//   sayHi();
-  
-//   let a = 3;
-// let b = new Number(3);
-// let c = 3;
-// console.log(a)
-// console.log(b)
-// console.log(c)
-// console.log(a == b);
-// console.log(a === b);
-// console.log(b === c);
-
-// function Person(firstName, lastName) {
-//   this.firstName = firstName;
-//   this.lastName = lastName;
-// }
-
-// const member = new Person('Lydia', 'Hallie');
-// Person.getFullName = function() {
-//   return `${this.firstName} ${this.lastName}`;
-// };
-
-// console.log(member.Person.getFullName());
